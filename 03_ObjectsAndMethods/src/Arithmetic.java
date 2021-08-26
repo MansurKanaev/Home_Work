2ada844a0265cb6d303be776f5f22da5e46b1538
Mansur <manskr98@mail.ru>
2021-08-26 12:20:36 +0300

    added 2 parameters

--- 03_ObjectsAndMethods/src/Arithmetic.java
+++ 03_ObjectsAndMethods/src/Arithmetic.java
@@ -1,35 +1,59 @@
 public class Arithmetic {
-    public static void main(String[] args) {
-        int x = 3;
-        int y = 3;
-        int sum;
-        int addition;
-        int c = x + y;
-        sum = c;
-        System.out.println("Сумма двух переменных равна:" + sum);
-        int d = x * y;
-        addition = d;
-        System.out.println("Произведение двух переменных:" + addition);
+    public static String items = "";
+    private static int sum;
+    private static int addition;
+    private static int maxNumber;
+    private static int minNumber;
 
+    public static void main(String[] args) {
+        variables(2,2);
+
+    }
+
+    public static void variables(int x, int y)
+    {
+
+        items = items + "\n" + "Переменная х=" + x + "\n" + "Переменная у=" + y;
+        sum = x + y ;
+        addition = x * y;
+        System.out.println(items);
+        System.out.println("Сумма двух переменных равна:"+ sum);
+        System.out.println("Произведение двух переменных равна:"+ addition);
         if (x == y) {
 
             System.out.println("Переменные равны:" + x);
             return;
-
         }
+        if(x>y) {
+         maxNumber=x;
+            System.out.println("Максимальное из двух чисел:" +maxNumber );
 
-        if (x > y) {
-
-            System.out.println("Максимальное из двух чисел:" + x);
-        } else {
-            System.out.println("Максимальное из двух чисел:" + y);
-        }
-        if (x < y) {
-            System.out.println("Минимальное  из двух чисел:" + x);
         }else {
-            System.out.println("Минимальное  из двух чисел:" + y);
-        }
+            System.out.println("Максимальное из двух чисел:" +y );
+
+    }
+            if(y<x){
+                minNumber = y;
+                System.out.println("Минмальное  из двух чисел:" + minNumber );
+            }else {
+                System.out.println("Минмальное  из двух чисел:" + x );
+            }
+
+
+
+
     }
 
 
-}
+
+
+
+    }
+
+
+
+
+
+
+
+
