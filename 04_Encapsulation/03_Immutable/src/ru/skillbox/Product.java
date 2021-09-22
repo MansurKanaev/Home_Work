a8200d804c9a09ca34be6a1b57d5aa4060cfb6e2
Mansur <manskr98@mail.ru>
2021-09-22 12:20:04 +0300

    4.7.2

--- 04_Encapsulation/03_Immutable/src/ru/skillbox/Product.java
+++ 04_Encapsulation/03_Immutable/src/ru/skillbox/Product.java
@@ -3,21 +3,21 @@ package ru.skillbox;
 
 public class Product {
     private final  String name;
+    private final int barCode;
+    private  int price;
 
-    public int barCode;
-
-    public Product(String name, int price) {
+    public Product(String name, int barCode) {
         this.name = name;
-        this.price = price;
+        this.barCode= barCode;
     }
-
     public String getName() {
         return name;
     }
-
     public int getPrice() {
         return price;
     }
-    private   int price;
+    public int getBarCode() {
+        return barCode;
+    }
 
 }
