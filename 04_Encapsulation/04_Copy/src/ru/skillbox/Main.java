3d711750e49e82006a8d3d9e8ac3f803f6c8cc3d
Mansur <manskr98@mail.ru>
2021-09-23 17:10:16 +0300

    4.10

--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -3,5 +3,13 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+        CourierService courierService = new CourierService(50,"Ул. Мотросова № 52",
+                "Товар хрупкий.","2345321");
+        Dimensions dimensions = new Dimensions();
+        dimensions.cargoVolume(25,15,35);
+
+
+
+
     }
 }
