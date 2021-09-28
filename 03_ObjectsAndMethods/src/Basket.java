60664f76ea4f441a5fe588407f2b74220bb3315e
Mansur <manskr98@mail.ru>
2021-09-28 23:49:37 +0300

    hw 5.1

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -1,71 +1,80 @@
 public class Basket {
 
     private static int count = 0;
+    private static double totalPrice = 0; // 1ая статистическая переменная общая стоимость всех товаров
+    private static int totalCount = 0; // 2ая статистическая переменная  количесвто всех товаров
+    private double totalWeight = 0; // общий вес товаров
+    private static double averagePrice = 0; // средняя цена товара в корзине
+    private static double averageCount = 0; // средняя стоимость корзины
     private String items = "";
-    private double totalPrice = 0;
-    private double totalWeight = 0;
     private int limit;
+
+
 
     public Basket() {
         increaseCount(1);
         items = "Список товаров:";
         this.limit = 1000000;
     }
-
     public Basket( int limit){
             this();
             this.limit = limit;
         }
-
     public Basket(String items, int totalPrice) {
         this();
         this.items = this.items + items;
-
-
     }
-
-    public static int getCount() {
-        return count;
-    }
-
+    // статистический метод который увеличивет значения переменных при добавлении в корзину новых товаров
     public static void increaseCount(int count) {
         Basket.count = Basket.count + count;
     }
-
     public void add(String name, int price, double weight) {
         add(name, price, count, weight);
-    }
 
+    }
     public void add(String name, int price, int count, double weight) {
         boolean error = false;
-
-
         if (contains(name)) {
             error = true;
         }
-
         if (totalPrice + count * price >= limit) {
             error = true;
         }
-
         if (error) {
-            System.out.println("Error occurred :(");
+            System.out.println("ошибка, превышен лимит! ");
             return;
         }
+        items = items + "\n" + name + " - " + count + " шт. - " + price + " р.- " + weight + " г.";
+        totalPrice  = totalPrice + price * count;
+        totalWeight = totalWeight + weight * count;
+        totalCount = totalCount  + count;
+        // формула отношение общей стоимости всех корзин к общему количеству всех товаров
+        averagePrice = totalPrice / totalCount;
+        // формула отношение общей стоимости всех корзин к количеству корзин
+        averageCount = totalPrice / Basket.count;
 
-        items = items + "\n" + name + " - " +
-            count + " шт. - " + price + " р.- " + weight + " г.";
 
-        totalPrice = (totalPrice + price * count);
-        totalWeight= (totalWeight + weight * count);
     }
-
     public void clear() {
         items = "";
         totalPrice = 0;
-        totalWeight =0;
+        totalWeight = 0;
+        averagePrice =0;
+        averageCount= 0;
     }
-
+    // Статистический метод  возвращающий отношение общей стоимости всех корзин к общему количеству всех товаров
+    public static double getAveragePrice(){
+        return averagePrice;
+    }
+    public static   double getAverageCount(){
+        return  averageCount;
+    }
+    public static int getCount() {
+        return count;
+    }
+    public static int getTotalCount(){
+        return totalCount;
+    }
     public int getTotalPrice() {
         return (int) totalPrice;
     }
@@ -75,16 +84,20 @@ public class Basket {
     public boolean contains(String name) {
         return items.contains(name);
     }
-
     public void print(String title){
             System.out.println(title);
-
             if (items.isEmpty()) {
                 System.out.println("Корзина пуста");
             } else {
                 System.out.println(items);
-                System.out.println("Общая сумма товаров:" + getTotalPrice()+"р.");
-                System.out.println("Общий вес товаров:"+ getTotalWeightPrice()+"г.");
+                System.out.println(
+                        "Общая цена всех товаров: " + getTotalPrice() + " р." + "\n" +
+                        "Общий вес всех товаров: " + getTotalWeightPrice() + " г." + "\n" +
+                        "Общее количество всех товаров: " + getTotalCount() + " шт." +"\n" +
+                        "Средняя цена товара в  корзине: " + getAveragePrice() + " р." + "\n" +
+                        "Средняя стоимость корзины: " + getAverageCount() + " р.");
+                        System.out.println();
+
             }
 
     }
--- 03_ObjectsAndMethods/src/Main.java
+++ 03_ObjectsAndMethods/src/Main.java
@@ -3,11 +3,32 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Basket basket = new Basket();
-        basket.add("Молоко",0 ,0 ,0 );
-        basket.add("Хлеб", 0, 0,0 );
-        basket.add("Макароны", 0, 0, 0);
-        basket.print("");
+        Basket dimaBasket = new Basket();
+        System.out.print("Корзина Димы: ");
+        dimaBasket.add("Молоко",80,3 ,500 );
+        dimaBasket.add("Хлеб", 30, 2,200 );
+        dimaBasket.add("Макароны", 50, 3, 350);
+        dimaBasket.print("");
+
+        Basket olyaBasket = new Basket();
+        System.out.print("Корзина Оли: ");
+        olyaBasket.add("Сыр",40,2,150);
+        olyaBasket.add("Сок",80, 3, 180);
+        olyaBasket.print("");
+
+        Basket genaBasket = new Basket();
+        System.out.print("Корзина Гены: ");
+        genaBasket.add("Арбуз", 235,1,1235);
+        genaBasket.add("Курица",300,2,1540);
+        genaBasket.print("");
+
+
+
+
+
+
+
+
 
 
         // Arithmetic класс
