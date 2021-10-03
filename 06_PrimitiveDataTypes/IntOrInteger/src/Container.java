a07acc465c5512c3d8ec1f33fd1f6bf68ead9452
Mansur <manskr98@mail.ru>
2021-10-03 12:27:46 +0300

    6 module HW 1,2

--- 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -1,11 +1,13 @@
 public class Container {
-    private Integer count;
+
+    private int count; // поменял Класс обертку Integer, на примитив int.
 
     public void addCount(int value) {
         count = count + value;
     }
-
     public int getCount() {
         return count;
     }
+
+
 }
--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -8,5 +8,22 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+        for(int i = 1040; i <= 1045 ; i++){
+            char c = (char) i;
+            System.out.println(i + " - " + c);
+        }
+        System.out.println("1025" + " - " + (char) 1025);
+
+        for(int i = 1046; i <= 1077 ; i++){
+            char c = (char) i;
+            System.out.println(i + " - " + c);
+        }
+        System.out.println("1105" + " - " + (char) 1105);
+
+        for(int i = 1078; i <= 1103 ; i++){
+            char c = (char) i;
+            System.out.println(i + " - " + c);
+        }
+
     }
 }
