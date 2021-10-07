5a1b441acdfa16c6c95aed1a2ff80ebc628871c5
Mansur <manskr98@mail.ru>
2021-10-07 23:11:17 +0300

    HW 7.2

--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -5,6 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
+        int numBoxes = Integer.parseInt(boxes);
+        int truck = 0;
+        int container = 0;
+        for (int i = 1; i <=numBoxes; i++){
+         if(i % 324 == 1){
+           truck += 1;
+           System.out.println("Грузовик: " + truck);
+         } if(i % 27 == 1){
+             container += 1;
+                System.out.println("\tКонтейнер: " + container);
+            } System.out.println("\t\tЯщик: " + i);
+        }
+        System.out.println("Необходимо:" + System.lineSeparator() + "грузовиков - " + truck + " шт." +
+                System.lineSeparator() + "контейнеров - " + container + " шт." );
 
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
