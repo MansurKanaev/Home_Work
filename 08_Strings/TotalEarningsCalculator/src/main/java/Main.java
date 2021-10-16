bade2b5c3198cc3548472ae66fe3355401aed06f
Mansur <manskr98@mail.ru>
2021-10-16 19:41:42 +0300

    module 8 HW 1

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,31 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+        String text = "Вася заработал  5000 рублей, Петя  7563 рубля, а Маша - 30000 рублей";
+        //TODO: напишите ваш код, результат вывести в консоль
 
+        System.out.println(getWage(text));
+    }
+
+    public static String getWage(String text) {
+        String vasyStart = "Вася заработал";
+        String petyStart = "Петя";
+        String mashaStart = "Маша -";
+        String end = "руб";
+
+        int newVasyStart = text.indexOf(vasyStart) + vasyStart.length();
+        int newPetyStart = text.indexOf(petyStart) + petyStart.length();
+        int newMashaStart = text.indexOf(mashaStart) + mashaStart.length();
+
+        int endVasy = text.indexOf(end, newVasyStart);
+        int endPety = text.indexOf(end, newPetyStart);
+        int endMasha = text.indexOf(end, newMashaStart);
+
+        int sumVasy = Integer.parseInt(text.substring(newVasyStart, endVasy).trim());
+        int sumPety = Integer.parseInt(text.substring(newPetyStart, endPety).trim());
+        int sumMasha = Integer.parseInt(text.substring(newMashaStart, endMasha).trim());
+
+        return String.valueOf(sumVasy + sumPety + sumMasha);
+    }
 }
