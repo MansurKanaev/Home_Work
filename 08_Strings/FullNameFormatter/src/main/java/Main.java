cf11a282b13b562f8dbfe786ede9e0ac74a8d95d
Mansur <manskr98@mail.ru>
2021-10-16 20:35:17 +0300

    8 Module HW 2

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -2,17 +2,91 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            String surname = "";
+            String name = "";
+            String patronymic = "";
+            String fullName = "";
+            int amountText = 0;
+            int amountSymbol = 0;
+            int numberSpaces = 0;
+
+            //Проверка строки на допустимые символы;
+            for (int i = 0; i < input.length(); i++) {
+                String symbol = input.substring(i, i + 1);
+                char s = symbol.charAt(0);
+                for (int x = 0; x < 65536; x++) {
+                    char c = (char) x;
+                    if (c >= 'А' && c <= 'я' || c >= 'A' && c<= 'z'|| c == 'ё' || c == 'Ё' || c == '-' || c == ' ') {
+                        if (s == c) {
+                            amountSymbol += 1;
+                        }
+                    }
+                }
+            }
+            //Считаем количество пробелов (логика: если их количество равно 2ум, значит слов - 3);
+            for (int i = 0; i < input.length(); i++) {
+                String symbol = input.substring(i, i + 1);
+                if (symbol.equals(" ")) {
+                    numberSpaces += 1;
+                }
+            }
+
+            //Вывод ошибки, если количество пробелов не соответствует или введены недопустимые символы;
+            if (!(numberSpaces == 2) || !(amountSymbol == input.length())) {
+                System.out.println("Введенная строка не является ФИО");
+                break;
+            }
+
+
+            //Печатаем фамилию;
+            while (amountText < input.length()) {
+                String symbol = input.substring(amountText, amountText + 1);
+                amountText = amountText + 1;
+                if (!symbol.equals(" ")) {
+                    surname += symbol;
+                } else {
+                    break;
+                }
+            }
+
+            //Печатаем имя;
+            while (amountText < input.length()) {
+                String symbol = input.substring(amountText, amountText + 1);
+                amountText += 1;
+                if (!symbol.equals(" ")) {
+                    name = name + symbol;
+                } else {
+                    break;
+                }
+            }
+
+            //Печатаем отчество;
+            while (amountText < input.length()) {
+                String symbol = input.substring(amountText, amountText + 1);
+                amountText = amountText + 1;
+                patronymic += symbol;
+            }
+            //Если один из элементов пуст - вывод ошибки
+            if (surname.isEmpty() || name.isEmpty() || patronymic.isEmpty()) {
+                System.out.println("Введенная строка не является ФИО");
+                break;
+            }
+
+            //Формируем строку вывода
+            fullName = "Фамилия: " + surname + System.lineSeparator() +
+                    "Имя: " + name + System.lineSeparator() + "Отчество: " + patronymic;
+            System.out.println(fullName);
+        }
     }
-  }
-
 }
+//TODO:напишите ваш код тут, результат вывести в консоль.
+//При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
