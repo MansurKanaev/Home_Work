6e1020f16bbe75dc7550962a8623c23bad6ee4a5
Mansur <manskr98@mail.ru>
2021-10-18 21:33:10 +0300

    HW 3

--- 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
@@ -1,12 +1,25 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+    }
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
-
+    public static int calculateSalarySum(String text) {
+       String result = text.replaceAll("[^0-9,]","");
+       int salaryAmount = 0;
+       String[] salary;
+       salary = result.split(",");
+       if(result.isEmpty()){
+           return 0;
+       }
+       for(String s : salary){
+           salaryAmount += Integer.parseInt(s);
+       }
+        return salaryAmount;
+    }
 }
