365232e65e42109f8e3a14ee28e93ed1e8b8a1c5
Mansur <manskr98@mail.ru>
2021-10-18 23:16:56 +0300

    HW 4

--- 08_Strings/SplitText/src/main/java/Main.java
+++ 08_Strings/SplitText/src/main/java/Main.java
@@ -3,10 +3,16 @@ public class Main {
   public static void main(String[] args) {
 
   }
-
   public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
+    String fullText = text.replaceAll("[0-9\\.,-:;]", " ");
+    String result = "";
+    String[] words = fullText.split("\\s+");
+    if(fullText.isEmpty()){
+      return "";
+    }
+    for(int i = 0; i < words.length; i++) {
+     result += words[i] + System.lineSeparator();
+    }
+    return result.trim();
   }
-
 }
