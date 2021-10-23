467b5eb2fbc7ef8f5a708558d98cd93d7ecb1249
Mansur <manskr98@mail.ru>
2021-10-23 23:20:10 +0300

    hw 9 1.1

--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        String line = "Каждый охотник желает знать, где сидит фазан";
+        String[] line = {"1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 "};
+
+        for (int i = line.length - 1; i >= 0; i--) {
+            System.out.println(line[i]);
+        }
     }
 }
--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
@@ -1,7 +1,21 @@
 public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
-    public static String[] reverse (String[] strings){
+    public static String[] reverse(String[] strings) {
+
+        int symbols = strings.length;
+        String endSigns;
+
+        for (int i = 0; i < symbols / 2; i++) {
+            endSigns = strings[symbols - i - 1]; // берем  значения с конца
+            strings[symbols - i - 1] = strings[i]; // берем значения с начала
+            strings[i] = endSigns; // меняем  начальные значения на конечные
+        }
+
+        for (String string : strings) {
+            System.out.println(string); // формируем обратный вывод символов из массива string[n]
+        }
         return strings;
     }
 }
+
