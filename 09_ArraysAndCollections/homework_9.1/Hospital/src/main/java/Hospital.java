be328cc31ef3fb39942da04ae2881a8eb84610f1
Mansur <manskr98@mail.ru>
2021-10-25 13:39:51 +0300

    hw 9 1.2

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -1,23 +1,36 @@
 public class Hospital {
-
+    //TODO: напишите метод генерации массива температур пациентов
     public static float[] generatePatientsTemperatures(int patientsCount) {
+        float a = 32.0f;
+        float b = 40.0f;
+        float[] temp = new float[patientsCount];
 
-        //TODO: напишите метод генерации массива температур пациентов
-
-        return new float[0];
+        for (int i = 0; i < patientsCount; i++) {
+            temp[i] = a + (float) (Math.round((Math.random() * b)) / 10.0);
+        }
+        return temp;
     }
 
+    /*
+           TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
+               а также температуры всех пациентов.
+           */
     public static String getReport(float[] temperatureData) {
-        /*
-        TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
-            а также температуры всех пациентов.
-        */
+        int healthyPatients = 0;  // Здоровые пациенты
+        double averageTemperature = 0.0;  // Средняя температура
+        StringBuilder patientTemperatures = new StringBuilder();
+//
+        for (float x : temperatureData) {
+            patientTemperatures.append(x).append(" ");
+            averageTemperature += x;
+            if (x > 36.1 && x < 37.0) {
+                healthyPatients += 1;
+            }
+        }
+        averageTemperature = (float) (Math.round(averageTemperature / temperatureData.length * 100.0)) / 100.0;
 
-        String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
-
-        return report;
+        return "Температуры пациентов: " + patientTemperatures.toString().trim() +
+                "\nСредняя температура: " + averageTemperature +
+                "\nКоличество здоровых: " + healthyPatients;
     }
 }
