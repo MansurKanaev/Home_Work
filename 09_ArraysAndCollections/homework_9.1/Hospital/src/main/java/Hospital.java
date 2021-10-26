9a5ab74d6aa23d4e8686d8072cedeffbb15bee44
Mansur <manskr98@mail.ru>
2021-10-26 11:38:38 +0300

    hw 9 1.2

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -1,12 +1,12 @@
 public class Hospital {
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
-        float a = 32.0f;
-        float b = 40.0f;
+        float a = 32.0F;
+        float b = 40.0F;
         float[] temp = new float[patientsCount];
 
         for (int i = 0; i < patientsCount; i++) {
-            temp[i] = a + (float) (Math.round((Math.random() * b)) / 10.0);
+            temp[i] = a + (float) (Math.round((Math.random() * b) * 2.0) / 10.0);
         }
         return temp;
     }
@@ -19,7 +19,7 @@ public class Hospital {
         for (float x : temperatureData) {
             patientTemperatures.append(x).append(" ");
             averageTemperature += x;
-            if (x > 36.1 && x < 37.0) {
+            if (x >= 36.2F && x <= 36.9F) {
                 healthyPatients += 1;
             }
         }
