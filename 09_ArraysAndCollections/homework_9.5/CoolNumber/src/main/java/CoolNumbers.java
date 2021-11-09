6a84286ec42e4b78326b89d60ba79cec1680fd3c
Mansur <manskr98@mail.ru>
2021-11-09 23:26:14 +0300

    hw 9.5

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,117 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
+    public static ArrayList<String> list = new ArrayList<>();
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        ArrayList<String> coolNumbersListWithoutRegion = new ArrayList<>();
+        String numberplate;
+        String numberplateWithoutRegion;
+
+        char x; // первая буква
+        char z; // вторая буква
+        char y; // третья буква
+
+        ArrayList<Character> allowedLetters = new ArrayList<>() {{
+            add('А');
+            add('В');
+            add('Е');
+            add('К');
+            add('М');
+            add('Н');
+            add('О');
+            add('Р');
+            add('С');
+            add('Т');
+            add('У');
+            add('Х');
+        }};
+
+        Collections.shuffle(allowedLetters, new Random());
+
+        for (Character firstLetter : allowedLetters) {
+            x = firstLetter;
+            for (int n = 0; n < 10; n++) {
+                for (Character secondLetter : allowedLetters) {
+                    z = secondLetter;
+                    for (Character thirdLetter : allowedLetters) {
+                        y = thirdLetter;
+                        numberplateWithoutRegion = String.format("%c%d%d%d%c%c", x, n, n, n, z, y);
+                        coolNumbersListWithoutRegion.add(numberplateWithoutRegion);
+                    }
+                }
+            }
+        }
+
+        Collections.shuffle(coolNumbersListWithoutRegion, new Random());
+
+        for (String numberWithoutRegion : coolNumbersListWithoutRegion) {
+            for (int j = 1; j <= 199; j++) {
+
+                if (j < 10) {
+                    numberplate = String.format("%s0%d", numberWithoutRegion, j);
+
+                } else {
+                    numberplate = String.format("%s%d", numberWithoutRegion, j);
+                }
+                list.add(numberplate);
+            }
+            if (list.size() >= 2_000_000) {
+                break;
+            }
+        }
+
+        Collections.shuffle(list, new Random());
+        return list;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
+        long time1 = System.nanoTime();
+        if (list.contains(number)) {
+            long time2 = System.nanoTime();
+            System.out.println("Поиск перебором: номер найден за " + (time2 - time1) + " нс");
+            return true;
+        }
+        long time2 = System.nanoTime();
+        System.out.println("Поиск перебором: номер найден за " + (time2 - time1) + " нс");
         return false;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
+        Collections.sort(sortedList);
+        long time1 = System.nanoTime();
+        if (Collections.binarySearch(sortedList, number) >= 0) {
+            long time2 = System.nanoTime();
+            System.out.println("Бинарный поиск: номер найден за " + (time2 - time1) + " нс");
+            return true;
+        }
+        long time2 = System.nanoTime();
+        System.out.println("Бинарный поиск: номер не найден за " + (time2 - time1) + " нс");
         return false;
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
+        long time1 = System.nanoTime();
+        if (hashSet.contains(number)) {
+            long time2 = System.nanoTime();
+            System.out.println("Поиск в HashSet: номер найден за " + (time2 - time1) + " нс");
+            return true;
+        }
+        long time2 = System.nanoTime();
+        System.out.println("Поиск в HashSet: номер не найден за " + (time2 - time1) + " нс");
         return false;
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
+        long time1 = System.nanoTime();
+        if (treeSet.contains(number)) {
+            long time2 = System.nanoTime();
+            System.out.println("Поиск в TreeSet: номер найден за " + (time2 - time1) + " нс");
+            return true;
+        }
+        long time2 = System.nanoTime();
+        System.out.println("Поиск в TreeSet: номер не найден за " + (time2 - time1) + " нс");
         return false;
     }
-
 }
--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -1,12 +1,17 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+
 public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
 
     public static void main(String[] args) {
-
+        List<String> list = CoolNumbers.generateCoolNumbers();
+        HashSet<String> list1 = new HashSet<>(list);
+        TreeSet<String> list2 = new TreeSet<>(list);
+        CoolNumbers.bruteForceSearchInList(list, "К999МР95");
+        CoolNumbers.binarySearchInList(list, "К999МР95");
+        CoolNumbers.searchInHashSet(list1, "К999МР95");
+        CoolNumbers.searchInTreeSet(list2, "К999МР95");
+        System.out.println("Всего красивых номеров: " + list.size());
     }
 }
