d2b43c04e83914291d5cbe1b957df4c6b5b32af5
Mansur <manskr98@mail.ru>
2022-02-11 00:23:38 +0300

    hw 15.2

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,46 @@
+import java.io.*;
+import java.util.Objects;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+        File fOrig = new File(sourceDirectory);
+        File fCop = new File(destinationDirectory);
+        if (fOrig.isDirectory()) {
+            if (!fCop.exists()) {
+                //noinspection ResultOfMethodCallIgnored
+                fCop.mkdir();
+            }
+            for (String file : Objects.requireNonNull(fOrig.list())) {
+                try {
+                    copyFolder(new File(fOrig, file).getPath(), new File(fCop, file).getPath());
+                } catch (NullPointerException e) {
+                    System.err.println("Не удалось скопировать файл \"" + file + "\"!");
+                }
+            }
+        } else {
+            if (fOrig.exists()) {
+                InputStream is = null;
+                OutputStream os = null;
+                try {
+                    is = new FileInputStream(fOrig);
+                    os = new FileOutputStream(fCop);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = is.read(buffer)) > 0) {
+                        os.write(buffer, 0, length);
+                    }
+                } finally {
+                    if (is != null && os != null) {
+                        is.close();
+                        os.close();
+                    }
+                }
+            }
+            if (fCop.exists() && fOrig.exists()) {
+                Main.status = true;
+            }
+        }
     }
 }
+
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+    public static boolean status = false;
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите путь до папки, для ее копирования:");
+            String sourceDirectory = scanner.nextLine();
+            System.out.println("Введите путь до папки куда копировать файлы:");
+            String destinationDirectory = scanner.nextLine();
+            try {
+                FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+                if (status) {
+                    System.out.println("Копирование завершено!");
+                } else {
+                    System.err.println("Ошибка введите запрос корректно!");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (sourceDirectory.isEmpty()) {
+                break;
+            }
+        }
     }
 }
