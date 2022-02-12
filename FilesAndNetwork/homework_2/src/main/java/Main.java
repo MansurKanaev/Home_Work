0358249813c7a04ad4cdecd68b44f55b23359bee
Mansur <manskr98@mail.ru>
2022-02-12 20:48:40 +0300

    dop_dz_15_2

--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,28 +1,38 @@
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.Scanner;
 
 public class Main {
     public static boolean status = false;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Введите путь до папки, для ее копирования:");
-            String sourceDirectory = scanner.nextLine();
-            System.out.println("Введите путь до папки куда копировать файлы:");
-            String destinationDirectory = scanner.nextLine();
-            try {
-                FileUtils.copyFolder(sourceDirectory, destinationDirectory);
-                if (status) {
-                    System.out.println("Копирование завершено!");
-                } else {
-                    System.err.println("Ошибка введите запрос корректно!");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            if (sourceDirectory.isEmpty()) {
-                break;
-            }
-        }
+        System.out.println("Введите путь до папки, для ее копирования:");
+        String sourceDirectory = scanner.nextLine();
+        System.out.println("Введите путь до папки куда копировать файлы:");
+        String destinationDirectory = scanner.nextLine();
+        Path source = Paths.get(sourceDirectory);
+        Path destination = Paths.get(destinationDirectory);
+        Files.walkFileTree(source, new walkFileTree.CopyingFileVisitor(source, destination));
+        System.out.println("Копирование завершено!");
+
+//        do {
+//            System.out.println("Введите путь до папки, для ее копирования:");
+//
+//            System.out.println("Введите путь до папки куда копировать файлы:");
+//
+//            try {
+//                FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+//                if (status) {
+//                    System.out.println("Копирование завершено!");
+//                } else {
+//                    System.err.println("Ошибка введите запрос корректно!");
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        } while (!sourceDirectory.isEmpty());
+
+
     }
 }
--- FilesAndNetwork/homework_2/src/main/java/walkFileTree.java
+++ FilesAndNetwork/homework_2/src/main/java/walkFileTree.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class walkFileTree {
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class walkFileTree {
+
+    public static class CopyingFileVisitor extends SimpleFileVisitor<Path> {
+        private final Path source;
+        private final Path destination;
+
+        public CopyingFileVisitor(Path source, Path destination) {
+            this.source = source;
+            this.destination = destination;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                throws IOException {
+            System.out.println("Процесс обработки директории: " + dir);
+            Path destinationPath = destination.resolve(source.relativize(dir));
+            if (!destinationPath.toFile().exists()) {
+                Files.createDirectory(destinationPath);
+            }
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            System.out.println("Файл скопирован: " + file);
+            Path destinationPath = destination.resolve(source.relativize(file));
+            Files.copy(file, destinationPath, StandardCopyOption.COPY_ATTRIBUTES, StandardCopyOption.REPLACE_EXISTING);
+            return FileVisitResult.CONTINUE;
+        }
+    }
 }
